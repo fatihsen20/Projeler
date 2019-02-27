8319b8bb99cad8629084652cdbc90c5b11737bab
Fatih Şen <31111697+fuzzyoxygen@users.noreply.github.com>
2019-02-27 09:27:54 +0300

    Pi sayısı hesaplama

---
+++ Java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int alt_deger=0,ust_deger=1;
+        double h=0.0001;
+        double sonuc=0;
+        double x;
+        for (double i = alt_deger; i <ust_deger ; i=i+h) {
+            sonuc+=h*(Math.sqrt(1-Math.pow(i,2)));
+
+        }
+        x=4*(sonuc);
+        System.out.println(x);
+
+
+    }
+}
