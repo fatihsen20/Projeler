786570ad221a9b5ba7179c32f14fb87a15a1ccb9
Fatih Şen <31111697+fuzzyoxygen@users.noreply.github.com>
2019-03-17 23:04:19 +0200

    Add files via upload

---
+++ Java/Palindrome/PolindromeTester.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class PolindromeTester {
+    public static void main(String[] args) {
+        Palindrome newobj=new Palindrome();
+        System.out.println(newobj.Polindromemethod("Aga"));
+    }
+}
